ecb7861ed67a85331d736a98f204ed1469b23b9c
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-09-09 00:17:22 +0600

    asd

---
+++ src/components/test.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+export const Test = () => {
+    return (
+        <>
+            <h1>Hello Test</h1>
+            <Link to="/">Home</Link>
+        </>
+    )
+}
