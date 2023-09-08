b3ec280794f6edac3c4b1927d78cee42fadd3cb4
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-09-08 20:10:01 +0600

    a lot

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Profiler, createContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Login, Register } from './pages/authentication'
 import { Profile } from './model'
 import { CurrentProfileContext, UsersContext } from './contextproviders'
 
+import ibosLogo from './assets/images/ibos-logo-210.webp'
+import mugshot from './assets/images/mugshot.png'
+
 function App() {
-  const [count, setCount] = useState(0)
   const [loggedinUser, setLogggedinUser] = useState<Profile>()
   const [registeredUsers, setRegisteredUser] = useState<Profile[]>([])
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
 
   useEffect(() => {
     const userData = localStorage.getItem("users")
@@ -22,6 +25,25 @@ function App() {
 
   return (
     <>
+      <nav className='h-20 flex justify-between items-center px-4 bg-gradient-to-r from-orange-300 to-blue-100'>
+        <div className="h-12">
+          <img src={ibosLogo} alt="IBos Logo" className='h-full' />
+        </div>
+        {
+          loggedinUser ? <button onClick={e => setShowDropdown((v) => !v)} id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" className={`h-12 items-center flex gap-2 py-2 px-4 rounded shadow ${showDropdown ? 'bg-blue-100 shadow-black' : 'bg-orange-300'} relative hover:shadow-black hover:bg-blue-100 hover:text-black active:shadow-none active:text-white active:bg-gray-500`}>
+            <img src={loggedinUser.profilePicture || mugshot} alt="" className='rounded-full h-full' />
+            <p>Welcome back {loggedinUser.firstname}</p>
+            {
+              showDropdown && <div className="absolute w-full top-[calc(100%+0.5rem)] right-0 ">
+                <button className='p-3 w-full bg-orange-300 rounded' onClick={() => setLogggedinUser(() => undefined)}>Sign out</button>
+              </div>
+            }
+          </button> : <a href='/auth' className='py-2 px-4 rounded shadow bg-orange-300 hover:shadow-black hover:bg-blue-100'>Join us</a>
+        }
+        <div id="dropdownHover" className="w-20 h-20 hidden bg-red-500 top-[calc(100%+0.5rem)] right-2">
+
+        </div>
+      </nav>
       <UsersContext.Provider value={{
         profiles: registeredUsers,
         setProfiles: p => setRegisteredUser(
--- src/model.ts
+++ src/model.ts
@@ -1,7 +1,21 @@
 export interface Profile {
     username: string,
     password: string,
-    firstname: string
-    lastname: string
-    userbio: string
+    firstname: string,
+    lastname: string,
+    userbio: string,
+    profilePicture?: string,
+}
+
+export enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH,
+}
+
+export interface Task {
+    title: string,
+    description: string,
+    dueDate: Date,
+    priority: TaskPriority
 }
